737f3eec7a3ee0a0d706696dda20d4390f8100a6
BcRikko <s.shin1106@gmail.com>
2024-04-16 22:39:54 +0900

    2.21 純粋関数のテスト

--- Chapter2/CoffeeBreak.hs
+++ Chapter2/CoffeeBreak.hs
@@ -1,6 +1,6 @@
 module CoffeeBreak where
 increment x = x + 1
 
-getFirstCharacter = head
+getFirstCharacter word = head word
 
 wordScore word = length word
--- Chapter2/TestCoffeeBreak.hs
+++ Chapter2/TestCoffeeBreak.hs
@@ -3,3 +3,24 @@
 module TestCoffeeBreak where
 import Test.HUnit
 import CoffeeBreak
+
+test_increment = TestCase $ do
+  assertEqual "increment 1" 2 (increment 1)
+  assertEqual "increment 0" 1 (increment 0)
+  assertEqual "increment -1" 0 (increment (-1))
+
+test_getFirstCharacter = TestCase $ do
+  assertEqual "getFirstCharacter abc" 'a' (getFirstCharacter "abc")
+  assertEqual "getFirstCharacter def" 'd' (getFirstCharacter "def")
+
+test_wordScore = TestCase $ do
+  assertEqual "wordScore \"abc\"" 3 (wordScore "abc")
+  assertEqual "wordScore \"def\"" 3 (wordScore "def")
+  assertEqual "wordScore \"\"" 0 (wordScore "")
+
+main =
+  runTestTT (test [
+    test_increment,
+    test_getFirstCharacter,
+    test_wordScore
+  ])
